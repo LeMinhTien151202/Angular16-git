5ce26a2298ffa5c451148ad69d397e90599d2ea9
LeMinhTien151202 <127039925+LeMinhTien151202@users.noreply.github.com>
2023-11-29 15:33:50 +0700

    for

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  
    public name = 'tien le';
    public age = 25;
+   public fruit = ['tao','cam','chuoi','chanh'];
+
+   public ngOnInit(): void {
+    console.log('trai cay: ', this.fruit);
+  }
+
 }

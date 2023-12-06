5e68cd93a7caa1d1e51cf21fbb91e4988ee37301
LeMinhTien151202 <127039925+LeMinhTien151202@users.noreply.github.com>
2023-12-06 16:37:35 +0700

    sd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'about', component: AboutComponent },
+{ path: 'home', component: HomeComponent },
+//khong có đường dẫn chạy đến home
+{path: '', component: HomeComponent },
+//nếu như sai đường dẫn hoặc ko có thì chạy đến pagenotfound
+{ path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

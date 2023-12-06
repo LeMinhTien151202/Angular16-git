dd36d5c8f990fc5692ad28fe3ee65991f91e31c2
LeMinhTien151202 <127039925+LeMinhTien151202@users.noreply.github.com>
2023-12-06 13:58:01 +0700

    ngfor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,11 @@ export class HomeComponent implements OnInit{
    public name = 'tien le';
    public age = 25;
    public fruit = ['tao','cam','chuoi','chanh'];
+   public fruit2 = [{fruit:'tao',gia:5000,haGia:true},
+   {fruit:'cam',gia:3000,haGia:true},
+   {fruit:'cam',gia:3000,haGia:true},
+   {fruit:'chuoi',gia:-5000,haGia:false}
+  ];
 
    public ngOnInit(): void {
     console.log('trai cay: ', this.fruit);
